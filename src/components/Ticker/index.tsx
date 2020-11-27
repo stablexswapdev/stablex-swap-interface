a0c1ae8d988a8c5e2457d5fc5ebc2b1e3436a965
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-11-27 18:14:58 +0800

    Update index.tsx

--- src/components/Ticker/index.tsx
+++ src/components/Ticker/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ScriptTag from 'react-script-tag';
+import {Helmet} from "react-helmet";
 
-const Ticker = (
-    <>
-        <ScriptTag type="text/javascript" src="https://files.coinmarketcap.com/static/widget/coinMarquee.js"/>
+const Ticker = props => (
+    <div>
+    <Helmet>
+              <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/coinMarquee.js" />
+            </Helmet>
         <div id="coinmarketcap-widget-marquee" coins="4943,4687,3408,825" currency="USD" theme="dark" transparent="true" show-symbol-logo="true"/>
-    </>
+    </div>
 );
 
 export default Ticker
